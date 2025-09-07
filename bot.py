efa8f4295ef94cdb616cef9aea603eb776c293a7
abenezer tefera <barkotasz@gmail.com>
2025-09-07 19:02:03 +0300

    json line fix

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import re
 import logging
+import json
 from datetime import datetime
 from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
